3d146baada911b9c06d504f41d945845f452c741
Jesper Reinholdt <jesperreinholdt@gmail.com>
2018-01-05 15:46:33 +0100

    added test check

--- setCookieTest.php
+++ setCookieTest.php
@@ -1,5 +1,14 @@
 <?php
 
+$test  = true;
+
+if($test){
+	$UserID = "test-uid"+intval( "0" . rand(1,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) ); 
+	if ($UserID){
+	setcookie("LoginUserID", $UserID, strtotime( '+90 days' ));  /* expire in 90 dasy */
+}
+} else
+
 $UserID = $_GET['partner-info']; 
 if ($UserID){
 setcookie("LoginUserID", $UserID, strtotime( '+90 days' ));  /* expire in 90 dasy */
