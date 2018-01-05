a64e4fda81c117abcda64d97f8f8e721e794a068
Jesper Reinholdt <jesperreinholdt@gmail.com>
2018-01-05 15:42:25 +0100

    Changed to use login user id

--- setCookieTest.php
+++ setCookieTest.php
@@ -1,7 +1,7 @@
 <?php
 
-$google_cid = $_GET['google_cid']; 
-if ($egoogle_cid){
-setcookie("_ga", $google_cid, strtotime( '+30 days' ));  /* expire in 7 dasy */
+$UserID = $_GET['partner-info']; 
+if ($UserID){
+setcookie("_ga", $UserID, strtotime( '+90 days' ));  /* expire in 90 dasy */
 }
 ?>
